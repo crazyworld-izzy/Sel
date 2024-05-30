7d0ce295b045e7da2af2d52e39e37791f55ff2a3
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-05-30 19:41:37 +0530

    Update bot_left.py

--- VIPMUSIC/plugins/tools/bot_left.py
+++ VIPMUSIC/plugins/tools/bot_left.py
@@ -8,11 +8,11 @@ from VIPMUSIC import app
 from VIPMUSIC.utils.database import delete_served_chat, get_assistant
 
 photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://telegra.ph/file/f69e1a6ecd625ebd3ab85.jpg",
+    "https://telegra.ph/file/f69e1a6ecd625ebd3ab85.jpg",
+    "https://telegra.ph/file/f69e1a6ecd625ebd3ab85.jpg",
+    "https://telegra.ph/file/f69e1a6ecd625ebd3ab85.jpg",
+    "https://telegra.ph/file/f69e1a6ecd625ebd3ab85.jpg",
 ]
 
 
