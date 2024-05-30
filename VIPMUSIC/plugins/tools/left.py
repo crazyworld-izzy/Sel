6b6a0ed9e73fa98961366fda2d29d782297f3abe
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-05-30 19:30:29 +0530

    Update left.py

--- VIPMUSIC/plugins/tools/left.py
+++ VIPMUSIC/plugins/tools/left.py
@@ -9,11 +9,11 @@ from pyrogram.types import ChatMemberUpdated, InlineKeyboardButton, InlineKeyboa
 from VIPMUSIC import app
 
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://telegra.ph/file/f69e1a6ecd625ebd3ab85.jpg",
+    "https://telegra.ph/file/f69e1a6ecd625ebd3ab85.jpg",
+    "https://telegra.ph/file/f69e1a6ecd625ebd3ab85.jpg",
+    "https://telegra.ph/file/f69e1a6ecd625ebd3ab85.jpg",
+    "https://telegra.ph/file/f69e1a6ecd625ebd3ab85.jpg",
 ]
 
 bg_path = "VIPMUSIC/assets/userinfo.png"
